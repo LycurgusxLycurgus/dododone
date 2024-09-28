99506e807f1620595f383f464e102c93e0a4330b
LycurgusxLycurgus <Extermporeeducacion@gmail.com>
2024-09-28 05:51:24 -0500

    cors_fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: '*', // For development, allow all origins. In production, specify your frontend URL.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
